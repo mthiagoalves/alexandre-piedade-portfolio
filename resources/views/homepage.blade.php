e155d84a2bff4a6d18bed28a3029b50057fd9a63
mthiagoalves <m.thiago.alves@live.com>
2023-10-18 10:43:15 +0100

    feat(homepage): add function to remove blue from banner

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 @section('meta')
-
 @endsection
 
 @section('style-include')
-<style>
+    <style>
 
-</style>
+    </style>
 @endsection
 
 @section('main-content')
-    
+    <script>
+        document.querySelector('.blur').addEventListener('click', function() {
+            var image = document.querySelector('.blur');
+            if (image.classList.contains('click-blur')) {
+                image.classList.remove('click-blur');
+            } else {
+                image.classList.add('click-blur');
+            }
+        });
+    </script>
 @endsection
-
-
