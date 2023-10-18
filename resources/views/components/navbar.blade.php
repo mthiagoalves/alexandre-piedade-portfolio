41f0191b5e0275858ebdafdec9aa519f30a48e55
mthiagoalves <m.thiago.alves@live.com>
2023-10-18 10:43:32 +0100

    feat(nav): finished banner with blur

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,20 +7,21 @@
         </a>
         <ul class="nav justify-content-end">
             <li class="nav-item mx-3">
-                <a class="p-2" aria-current="page" href="{{ route('pageAbout') }}">About</a>
+                <a class="p-2 px-3" aria-current="page" href="{{ route('pageAbout') }}">About</a>
             </li>
             <li class="nav-item mx-3">
-                <a class="p-2" href="{{ route('pageWork') }}">Work</a>
+                <a class="p-2 px-3" href="{{ route('pageWork') }}">Work</a>
             </li>
         </ul>
     </div>
 </nav>
 <div class="container-fluid p-0">
     <div class="col-12 p-0 position-relative">
-        <img src="https://dummyimage.com/1920x750/292a2f/0011ff" alt="banner" class="img-fluid">
+        <div class="blur"></div>
+        <img src="https://dummyimage.com/1920x1000/292a2f/0011ff" alt="banner" class="img-fluid initial-banner">
         <div class="located-in-pt">
             <div class="img-located-in-pt">
-                <img src="{{asset('/img/world.png')}}" alt="world" class="img-fluid rotate-image">
+                <img src="{{ asset('/img/world.png') }}" alt="world" class="img-fluid rotate-image">
             </div>
             <div class="text-located-in-pt">
                 <p>
@@ -28,6 +29,10 @@
                 </p>
             </div>
         </div>
-
+        <div class="text-name-main-banner">
+            <p>
+                Piedade - Alexandre
+            </p>
+        </div>
     </div>
 </div>
