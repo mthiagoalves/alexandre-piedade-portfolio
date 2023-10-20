4f252ab4699ddad75859c7f960ff912d10f2428f
mthiagoalves <m.thiago.alves@live.com>
2023-10-20 17:10:32 +0100

    change banner initial and finished upload banner modal with preview image in imput

--- resources/views/backoffice/components/content-modals/initial-banner.blade.php
+++ resources/views/backoffice/components/content-modals/initial-banner.blade.php
@@ -1,3 +1,12 @@
+<style>
+    @media (min-width: 576px) {
+        .col-sm-3 {
+            flex: 0 0 auto;
+            width: 27.8888888%;
+        }
+    }
+</style>
+
 <button x-data="" x-on:click.prevent="$dispatch('open-modal', 'change-banner-initial')">
     Initial Banner <i class="fa-solid fa-plus fontawesome-icons"></i>
 </button>
@@ -10,12 +19,27 @@
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('You wanna change initial banner?') }}
         </h2>
-
-        <div class="mt-6">
-            <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
-
-            <x-text-input id="password" name="password" type="file" class="mt-1 block w-3/4"
-                placeholder="{{ __('Password') }}" />
+        <div class="container mt-3">
+            <div class="row justify-content-around">
+                <div class="col-sm-8 col-12 imgUp">
+                    <div class="imagePreview">
+                        <img src="/img/homepage/banner-initial.png" class="img-fluid" alt="">
+                    </div>
+                    <label class="btn btn-light">
+                        Upload Desktop<input type="file" class="uploadFile img" value="Upload Photo"
+                            style="width: 0px;height: 0px;overflow: hidden;" accept="image/*">
+                    </label>
+                </div>
+                <div class="col-sm-3 col-12 imgUp">
+                    <div class="imagePreview">
+                        <img src="/img/homepage/banner-initial-m.png" class="img-fluid" alt="">
+                    </div>
+                    <label class="btn btn-light">
+                        Upload Mobile<input type="file" class="uploadFile img" value="Upload Photo"
+                            style="width: 0px;height: 0px;overflow: hidden;" accept="image/*">
+                    </label>
+                </div>
+            </div>
         </div>
 
         <div class="mt-6 flex justify-end">
@@ -29,3 +53,39 @@
         </div>
     </form>
 </x-modal>
+
+<script>
+    const imageInputs = document.querySelectorAll('.uploadFile');
+
+    imageInputs.forEach(function(inputElement) {
+        inputElement.addEventListener('change', function() {
+            previewImages2(this);
+        });
+    });
+
+    function previewImages2(inputElement) {
+        const preview = inputElement.closest('.imgUp').querySelector('.imagePreview');
+        preview.innerHTML = '';
+
+        const files = inputElement.files;
+
+        function readAndPreview(file) {
+            if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+                const reader = new FileReader();
+
+                reader.addEventListener('load', function() {
+                    const image = new Image();
+                    image.src = this.result;
+                    image.classList.add('img-fluid'); // Adicione a classe "img-fluid"
+                    preview.appendChild(image);
+                });
+
+                reader.readAsDataURL(file);
+            }
+        }
+
+        if (files) {
+            Array.from(files).forEach(readAndPreview);
+        }
+    }
+</script>
