3865574d81e2f6c2fd4d0edcbdd9ee213f6ac909
mthiagoalves <m.thiago.alves@live.com>
2023-10-27 17:41:45 +0100

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            output: "dist", // Especifica o diretório de saída
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "dist", // Diretório de saída para arquivos de build
+    },
 });
